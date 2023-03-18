d0e76e8c458e79048c35e26877b388d9181346a5
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-18 23:47:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,62 +8,50 @@ const potterDateOfBirth = "31-07-1980";
 const potterAncestry = "half-blood";
 const potterEyeColour = "green";
 const potterHairColour = "black";
-const potterHogwartsStudent = true;
-const potterHogwartsStaff = false;
 const potterActor = "Daniel Radcliffe";
-const potterAlive = true;
 const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 
 // ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
 
 async function initApp() {
-    showCharacter(
-        potterImage,
-        potterName,
-        potterHouse,
-        potterGender,
-        potterDateOfBirth,
-        potterEyeColour,
-        potterHairColour,
-        potterAncestry,
-        potterActor
-    );
+    showCharacter(potterImage, potterName, potterHouse, potterDateOfBirth, potterActor);
 }
 
-function showCharacter(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor) {
-    document.querySelector("#characters").insertAdjacentHTML(
-        "beforeend",
-        /*html*/ `
-            <article>
-                <img src="${image}">
-                <h2>${name}</h2>
-                <p>${house}</p>
-            </article>
-        `
-    );
+// function showCharacter(image, name, house, dateOfBirth, actor) {
+//     document.querySelector("#characters").insertAdjacentHTML(
+//         "beforeend",
+//         /*html*/ `
+//             <article>
+//                 <img src="${image}">
+//                 <h2>${name}</h2>
+//                 <p>${house}</p>
+//                 <p>Date of Birth: ${dateOfBirth}</p>
+//                 <p>Played by ${actor}</p>
+//             </article>
+//         `
+//     );
+// }
 
-    document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
+function showCharacter(image, name, house, dateOfBirth, actor) {
+    const characterElement = document.createElement("article");
+    const imageElement = document.createElement("img");
+    const nameElement = document.createElement("h2");
+    const houseElement = document.createElement("p");
+    const dateOfBirthElement = document.createElement("p");
+    const actorElement = document.createElement("p");
 
-    function characterClicked() {
-        showCharacterModal(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor);
-    }
-}
+    imageElement.src = image;
+    nameElement.textContent = name;
+    houseElement.textContent = house;
+    dateOfBirthElement.textContent = `Date of Birth: ${dateOfBirth}`;
+    actorElement.textContent = `Played by ${actor}`;
 
-function showCharacterModal(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor) {
-    document.querySelector("#dialog-image").src = image;
-    document.querySelector("#dialog-title").textContent = name;
-    document.querySelector("#dialog-house").textContent = house;
+    characterElement.appendChild(imageElement);
+    characterElement.appendChild(nameElement);
+    characterElement.appendChild(houseElement);
+    characterElement.appendChild(dateOfBirthElement);
+    characterElement.appendChild(actorElement);
 
-    document.querySelector("#dialog-gender").textContent = gender;
-    document.querySelector("#dialog-birth-date").textContent = dateOfBirth;
-    document.querySelector("#dialog-eye-color").textContent = eyeColour;
-    document.querySelector("#dialog-hair-color").textContent = hairColour;
-    document.querySelector("#dialog-ancestry").textContent = ancestry;
-
-    document.querySelector("#dialog-name").textContent = name;
-    document.querySelector("#dialog-actor-name").textContent = actor;
-
-    // show dialog
-    document.querySelector("#dialog-character").showModal();
+    document.querySelector("#characters").appendChild(characterElement);
 }
