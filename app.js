ada9e528114a123a752dd42ef1c0f97bb6de1768
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-18 23:28:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const potterDateOfBirth = "31-07-1980";
 const potterAncestry = "half-blood";
 const potterEyeColour = "green";
 const potterHairColour = "black";
-const potterPatronus = "stag";
 const potterHogwartsStudent = true;
 const potterHogwartsStaff = false;
 const potterActor = "Daniel Radcliffe";
@@ -19,10 +18,20 @@ const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 window.addEventListener("load", initApp);
 
 async function initApp() {
-    showCharacter(potterImage, potterName, potterHouse);
+    showCharacter(
+        potterImage,
+        potterName,
+        potterHouse,
+        potterGender,
+        potterDateOfBirth,
+        potterEyeColour,
+        potterHairColour,
+        potterAncestry,
+        potterActor
+    );
 }
 
-function showCharacter(image, name, house) {
+function showCharacter(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor) {
     document.querySelector("#characters").insertAdjacentHTML(
         "beforeend",
         /*html*/ `
@@ -37,6 +46,24 @@ function showCharacter(image, name, house) {
     document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
 
     function characterClicked() {
-        showCharacterModal(character);
+        showCharacterModal(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor);
     }
+}
+
+function showCharacterModal(image, name, house, gender, dateOfBirth, eyeColour, hairColour, ancestry, actor) {
+    document.querySelector("#dialog-image").src = image;
+    document.querySelector("#dialog-title").textContent = name;
+    document.querySelector("#dialog-house").textContent = house;
+
+    document.querySelector("#dialog-gender").textContent = gender;
+    document.querySelector("#dialog-birth-date").textContent = dateOfBirth;
+    document.querySelector("#dialog-eye-color").textContent = eyeColour;
+    document.querySelector("#dialog-hair-color").textContent = hairColour;
+    document.querySelector("#dialog-ancestry").textContent = ancestry;
+
+    document.querySelector("#dialog-name").textContent = name;
+    document.querySelector("#dialog-actor-name").textContent = actor;
+
+    // show dialog
+    document.querySelector("#dialog-character").showModal();
 }
